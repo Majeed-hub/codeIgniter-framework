c011f8b5361ec5b42fb41cffa9106c4fc9c2675b
Majeed-hub <majeedbkl65@gmail.com>
2021-08-13 10:59:55 +0530

    small changes

--- application/controllers/Product.php
+++ application/controllers/Product.php
@@ -27,14 +27,14 @@ class Product extends CI_Controller
     public function edit()
     {
         $this->load->model('Product_model');
-        $id = $this->input->get("id");
+        $id = $this->input->get('id');
         $data['product'] = $this->Product_model->getDataForUpdate($id);
         $this->load->view('update_product', $data);
        
     }
     public function update(){
         $this->load->model('Product_model');
-        $id = $this->input->get("id");
+        $id = $this->input->get('id');
         $post_data = $this->input->post();
         $this->Product_model->update($post_data,$id);
         redirect('Product');
